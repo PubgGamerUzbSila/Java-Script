49dc3dda87d959b7944b8ed22983cb72fdbce492
Sattorov Kamronbek <PubgGamerUzbSila@gmail.com>
2023-07-26 08:17:52 +0500

    the end

---
+++ JavaScript_homeworks/28-dars_homework/main/app.js
@@ -0,0 +1,11 @@
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
